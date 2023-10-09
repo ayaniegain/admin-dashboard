ce44919cf3f2cdcdd317e5304a3bb066a0c28ab9
ayaniegain <ayaniegain@gmail.com>
2023-10-09 12:00:48 +0530

    feat: router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import './App.css'
+import Home from './pages/home/Home';
+import Products from './pages/products/Products';
+import Users from './pages/users/Users';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home/>
+      ),
+    },
+    {
+      path: "users",
+      element: <Users/>
+    },
+  {
+    path: "products",
+    element: <Products/>
+    },
+  ]);
+
   return (
-   <>
-   <h2>hello</h2>
-   </>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
 
